11ea5de226a4bbed559e4d0b0d88c40bde1081c9
Siobhán O'Hanlon <siobhanhanlon@outlook.com>
2022-01-17 09:45:57 +0000

    Adding how to add and display in arrays using struct

--- StructExample/Source.c
+++ StructExample/Source.c
@@ -23,4 +23,23 @@ void main()
 	scanf("%d", &p1.age);
 
 	printf("The first name is %s, the surname is %s and the age is %d", p1.name, p1.surname, p1.age);
+
+	//Reading into group
+	for (int i = 0; i < 5; i++)
+	{
+		printf("Please Enter First Name of Person at index %d:\t", i);
+		scanf("%s", group[i].name); //Dont need & for strings
+
+		printf("\nPlease Enter Surname of Person at index %d:\t", i);
+		scanf("%s", group[i].surname);
+
+		printf("\nPlease Enter Age of Person at index %d:\t", i);
+		scanf("%d", &group[i].age);
+	}
+
+	//Display
+	for (int j = 0; j < 5; j++)
+	{
+		printf("First Name: %s\tSurname: %s\tAge: %d\n", group[j].name, group[j].surname, group[j].age);
+	}
 }
